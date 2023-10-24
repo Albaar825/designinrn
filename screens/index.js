b78ea8634caf3e1a754d51edd21a34b418717ea6
Albaar825 <firdausalbar123@gmail.com>
2023-10-25 06:39:36 +0700

    praktikum design pab

---
+++ screens/index.js
@@ -0,0 +1,29 @@
+import LotsOfStyles from "./LotsOfStyles";
+import FixedDimensionsBasics from "./FixedDimensionsBasics";
+import FlexDimensionsBasics from "./FlexDimensionsBasics";
+import PercentageDimensionsBasics from "./PercentageDimensionsBasics";
+import FlexBasic from "./FlexBasics";
+import FlexDirectionBasics from "./FlexDirectionsBasics";
+import JustifyContentBasics from "./JustifyContentBasics";
+import AlignItemsLayout from "./AlignItemsLayouts";
+import WidthHeightBasics from "./WidthHeightBasics";
+import PositionLayout from "./PositionLayout";
+import DisplayAnImageWithStyle from "./DisplayAnImageWithStyle";
+import NativeBaseBasic from "./NativeBase";
+import GlueStackUIBasic from "./GlueStackBasic";
+
+export {
+  LotsOfStyles,
+  FixedDimensionsBasics,
+  FlexDimensionsBasics,
+  PercentageDimensionsBasics,
+  FlexBasic,
+  FlexDirectionBasics,
+  JustifyContentBasics,
+  AlignItemsLayout,
+  WidthHeightBasics,
+  PositionLayout,
+  DisplayAnImageWithStyle,
+  NativeBaseBasic,
+  GlueStackUIBasic,
+};
